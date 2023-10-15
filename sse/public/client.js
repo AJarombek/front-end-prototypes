bf87bb89ad4ee43874250eaeb14e3deba01c4aaf
ajarombek <ajarombek95@gmail.com>
2023-10-14 20:56:57 -0400

    example working

--- sse/public/client.js
+++ sse/public/client.js
@@ -5,9 +5,26 @@ Date: 10/13/2023
  */
 
 const sseOutput = document.getElementById('sse-output');
-const eventSource = new EventSource('http://localhost:8080/sse');
+let eventSource = new EventSource('http://localhost:8080/sse');
 
 eventSource.onmessage = (event) => {
     const message = event.data;
     sseOutput.innerHTML += '<p>' + message + '</p>';
+};
+
+eventSource.onopen = () => {
+    console.log('SSE connection opened');
+};
+
+eventSource.onerror = (error) => {
+    console.error('SSE error:', error);
+
+    // Implement reconnect logic here
+    setTimeout(() => {
+        // Close the existing connection
+        eventSource.close();
+
+        // Create a new connection
+        eventSource = new EventSource('http://localhost:8080/sse');
+    }, 3000);
 };
--- sse/server.js
+++ sse/server.js
@@ -1,3 +1,9 @@
+/*
+Basic Server-Send Events API server.
+Author: Andrew Jarombek
+Date: 10/14/2023
+ */
+
 const express = require('express');
 const http = require('http');
 
@@ -5,31 +11,50 @@ const app = express();
 const server = http.createServer(app);
 
 // Store connected clients
-const clients = [];
+let clients = [];
 
-// Create a route to initiate SSE
+// Initialize Server-Sent Events for a client
 app.get('/sse', (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
     res.flushHeaders();
 
+    const client = {
+        id: Date.now(),
+        response: res
+    }
+
     // Add the client to the list of connected clients
-    clients.push(res);
+    clients.push(client);
 
     // Send a welcome message
     res.write('data: Welcome to Server-Sent Events!\n\n');
 
     // Event handling for disconnect
     req.on('close', () => {
-        clients.splice(clients.indexOf(res), 1);
+        clients = clients.filter((c) => c.id !== client.id);
     });
 });
 
-// Broadcast messages to all connected clients
+// Get the number of connected clients
+// Example: curl http://localhost:8080/status
+app.get('/status', (req, res) => {
+    return res.json({clients: clients.length});
+});
+
+// Broadcast a message to all connected clients
+// Example: curl -X POST "http://localhost:8080/broadcast?message=Hello"
+app.post('/broadcast', (req, res) => {
+    const message = req.query.message;
+    broadcastMessage(message);
+    return res.json({message: 'Message Broadcasted'});
+});
+
 function broadcastMessage(message) {
     clients.forEach((client) => {
-        client.write(`data: ${message}\n`);
+        // Server-Sent Events messages are formatted as follows: data: <message>\n\n
+        client.response.write(`data: ${message}\n\n`);
     });
 }
 
